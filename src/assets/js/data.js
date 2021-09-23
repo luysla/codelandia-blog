9fc0a4aef4901b03a77c95a27ee087b2f0ba6927
luysla <dyana.tavares1@gmail.com>
2021-09-23 20:57:39 -0300

    Restructuring folders for deploy

---
+++ src/assets/js/data.js
@@ -0,0 +1,33 @@
+const news = [   
+	{
+		"id": 0,
+		"date": "15 de Jul, 2021",
+		"title": "Agora é oficial: o Windows 11 está vindo",
+		"description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius non libero, fugiat, perferendis itaque vitae corporis necessitatibus quo, placeat neque quas incidunt soluta veniam cumque sit esse iste perspiciatis quia.",
+		"loved": false
+	},
+	{
+		"id": 1,
+		"date": "15 de Jul, 2021",
+		"title": "Tim Berners-Lee vai leiloar código-fonte da web",
+		"description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius non libero, fugiat, perferendis itaque vitae corporis necessitatibus quo, placeat neque quas incidunt soluta veniam cumque sit esse iste perspiciatis quia.",
+		"loved": false
+	},
+	{
+		"id": 2,
+		"date": "15 de Jul, 2021",
+		"title": "Tem Firefox novo no pedaço e você vai querer migrar",
+		"description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius non libero, fugiat, perferendis itaque vitae corporis necessitatibus quo, placeat neque quas incidunt soluta veniam cumque sit esse iste perspiciatis quia.",
+		"loved": false
+	},
+	{
+		"id": 3,
+		"date": "15 de Jul, 2021",
+		"title": "John McAfee, criador do antivírus McAfee, morre",
+		"description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius non libero, fugiat, perferendis itaque vitae corporis necessitatibus quo, placeat neque quas incidunt soluta veniam cumque sit esse iste perspiciatis quia.",
+		"loved": false
+	}
+]
+
+export { news };
+
