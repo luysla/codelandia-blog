3287c1449b4331c37222c615418ef41978fe3f59
luysla <dyana.tavares1@gmail.com>
2021-09-30 22:40:22 -0300

    Adicionei função de like/dislike post

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,8 +1,43 @@
 import { news } from "./data.js";
 
-const card = document.querySelector('.card');
+const content = document.getElementById('content-card');
 
-console.log(card);
+const handleLike = (index) => {
 
-news.map((item, index) => {
+    if(news[index].loved) { 
+        news[index].loved = false 
+    } else { 
+        news[index].loved = true 
+    } 
+}
+
+content.innerHTML = news.map((item, index) => {
+    return(
+        `
+            <div class="card">
+                <div class="card__header">
+                    <p class="card__date">${item.date}</p>
+                    
+                    <button class="card__like">
+                        <i class="fa fa-heart-o card__icon"></i>
+                    </button>
+                    
+                </div>
+                <p class="card__title">${item.title}</p>
+                <p class="card__description">${item.description}</p>
+            </div>
+        `
+    )
+}).join('')
+
+const buttonLike = document.querySelectorAll('.card__like');
+
+buttonLike.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        handleLike(index);
+    })
 })
+
+
+
+
